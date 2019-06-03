e38ce9bbac0a2081702edfa10f9ba42eff8c7fea
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-06-03 20:48:40 +0200

    :sparkles: : add default value for external_url

--- src/main/java/io/codeka/gaia/bo/Settings.java
+++ src/main/java/io/codeka/gaia/bo/Settings.java
@@ -1,5 +1,6 @@
 package io.codeka.gaia.bo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ public class Settings {
     /**
      * Gaia's external url (used to allow runners to speak to Gaia)
      */
+    @Value("${:http://localhost:${server.port:8080}}")
     private String externalUrl;
 
     public String getExternalUrl() {
