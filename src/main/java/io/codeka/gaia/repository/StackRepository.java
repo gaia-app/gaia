c714098236ad5473267d03a6a512461d2ab25995
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-06-10 09:14:46 +0200

    :sparkles: : add stack information on index
    add :
    * running stack count
    * to update stack count

--- src/main/java/io/codeka/gaia/repository/StackRepository.java
+++ src/main/java/io/codeka/gaia/repository/StackRepository.java
@@ -1,12 +1,16 @@
 package io.codeka.gaia.repository;
 
 import io.codeka.gaia.bo.Stack;
+import io.codeka.gaia.bo.StackState;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 /**
  * Repository for stacks
  */
 @RepositoryRestResource
 public interface StackRepository extends MongoRepository<Stack, String>{
+    int countStacksByState(StackState state);
 }
