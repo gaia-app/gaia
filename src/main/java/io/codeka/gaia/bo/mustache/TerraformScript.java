0784d3bfb8518f04c9e930b3351a4ffbd357f491
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2019-06-18 21:12:42 +0200

    :recycle: : create mustache template to manage terraform scripts

---
+++ src/main/java/io/codeka/gaia/bo/mustache/TerraformScript.java
@@ -0,0 +1,58 @@
+package io.codeka.gaia.bo.mustache;
+
+/**
+ * Object matching the terraform mustache template
+ */
+public class TerraformScript {
+
+    private String gitRepositoryUrl;
+    private String gitDirectory;
+    private String externalUrl;
+    private String stackId;
+    private String command;
+
+    public String getGitRepositoryUrl() {
+        return gitRepositoryUrl;
+    }
+
+    public TerraformScript setGitRepositoryUrl(String gitRepositoryUrl) {
+        this.gitRepositoryUrl = gitRepositoryUrl;
+        return this;
+    }
+
+    public String getGitDirectory() {
+        return gitDirectory;
+    }
+
+    public TerraformScript setGitDirectory(String gitDirectory) {
+        this.gitDirectory = gitDirectory;
+        return this;
+    }
+
+    public String getExternalUrl() {
+        return externalUrl;
+    }
+
+    public TerraformScript setExternalUrl(String externalUrl) {
+        this.externalUrl = externalUrl;
+        return this;
+    }
+
+    public String getStackId() {
+        return stackId;
+    }
+
+    public TerraformScript setStackId(String stackId) {
+        this.stackId = stackId;
+        return this;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public TerraformScript setCommand(String command) {
+        this.command = command;
+        return this;
+    }
+}
