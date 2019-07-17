76b5d5ae9706574ba75574a2776954e613b11456
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-07-17 15:17:18 +0200

    :arrow_up: : updating to terraform 0.12.3

--- src/main/java/io/codeka/gaia/runner/StackRunner.java
+++ src/main/java/io/codeka/gaia/runner/StackRunner.java
@@ -60,7 +60,7 @@ public class StackRunner {
                     .build();
 
             // pull the image
-            dockerClient.pull("hashicorp/terraform:0.11.14");
+            dockerClient.pull("hashicorp/terraform:0.12.3");
 
             System.out.println("Create container");
             var containerCreation = dockerClient.createContainer(containerConfig);
