3e57d77ea25943f11910733f1bc952f312a0d2b9
Julien WITTOUCK <julien@codeka.io>
2019-08-26 19:27:37 +0200

    :children_crossing: : add variable validation to stack-vars component

--- src/main/resources/templates/vue_templates/stack-vars.vue
+++ src/main/resources/templates/vue_templates/stack-vars.vue
@@ -1,11 +1,17 @@
 <template id="stack-vars">
     <div>
         <div class="form-row align-items-end" v-for="(modVar,modVarIdx) in editableVars" v-if="editableVars.length > 0">
-            <div class="form-group col">
+            <b-form-group class="form-group col">
                 <label for="var-name">{{modVar.name}}: </label>
-                <input type="text" class="form-control" id="var-name" v-model="stack.variableValues[modVar.name]" @change="recomputeState" />
-                <small id="emailHelp" class="form-text text-muted">{{modVar.description}}</small>
-            </div>
+                <b-input id="var-name"
+                         v-model="stack.variableValues[modVar.name]"
+                         :state="validateVariable(modVar)"
+                         @update="recomputeState"
+                         aria-describedby="input-live-help input-live-feedback"
+                         trim></b-input>
+                <b-form-invalid-feedback id="input-live-feedback">This field is mandatory</b-form-invalid-feedback>
+                <b-form-text id="input-live-help">{{modVar.description}}</b-form-text>
+            </b-form-group>
         </div>
         <div v-if="editableVars.length === 0">
             <p>No editable variable defined for this module.</p>
@@ -19,14 +25,28 @@ Vue.component('stack-vars', {
     props: ["stack", "module"],
     methods: {
         recomputeState: function(){
-            if(stack.state === "RUNNING"){
-                stack.state = "TO_UPDATE";
+            if(this.stack.state === "RUNNING"){
+                this.stack.state = "TO_UPDATE";
             }
+            this.$emit("validated", this.variablesValid);
+        },
+        validateVariable: function(moduleVariable){
+            if(! moduleVariable.mandatory){
+                return null;
+            }
+            const value = this.stack.variableValues[moduleVariable.name];
+            return !(value === null || typeof value === "undefined" || value.length === 0);
         }
     },
     computed: {
         editableVars: function() {
             return this.module.variables.filter(variable => variable.editable);
+        },
+        variablesValid: function(){
+            return this.editableVars.every(moduleVar => {
+                const valid = this.validateVariable(moduleVar);
+                return valid === null || valid === true;
+            });
         }
     }
 });
