037ec7c9d6eefd14ef0796b4cd500490bbb73522
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-12-17 13:25:00 +0100

    :sparkles: : manage CSS & JS external dependencies dynamically

---
+++ src/main/java/io/codeka/gaia/dashboard/controller/UIExtensionsControllerAdvice.kt
@@ -0,0 +1,55 @@
+package io.codeka.gaia.dashboard.controller
+
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ModelAttribute
+
+/***
+ * A controller advice to inject ui-extensions
+ */
+@ControllerAdvice
+class UIExtensionsControllerAdvice(uiExtensions: List<UiExtension>) {
+
+    private var cssExtensions: List<UiExtension> = uiExtensions.filter { it.resourcePath.endsWith(".css") }
+
+    private var jsExtensions: List<UiExtension> = uiExtensions.filter { it.resourcePath.endsWith(".js") }
+
+    @ModelAttribute(value = "cssExtensions", binding = false)
+    fun getCssExtensions() = this.cssExtensions
+
+    @ModelAttribute(value = "jsExtensions", binding = false)
+    fun getJsExtensions() = this.jsExtensions
+
+}
+
+data class UiExtension(val resourcePath: String)
+
+@Configuration
+open class UIExtensionsConfig(){
+
+    @Bean
+    open fun uiExtensions(applicationContext: ApplicationContext): List<UiExtension>? {
+        val scanner = UiExtensionsScanner(applicationContext)
+        return scanner.scan(
+                // CSS
+                "classpath*:/**/bootstrap/**/bootstrap.min.css",
+                "classpath*:/**/font-awesome/**/all.css",
+                "classpath*:/**/bootstrap-vue/**/bootstrap-vue.min.css",
+                "classpath*:/**/vue-multiselect/**/vue-multiselect.min.css",
+
+                // JS
+                "classpath*:/**/jquery/**/jquery.min.js",
+                "classpath*:/**/popper.js/**/umd/popper.min.js",
+                "classpath*:/**/bootstrap/**/bootstrap.min.js",
+                "classpath*:/**/vue/**/vue.min.js",
+                "classpath*:/**/bootstrap-vue/**/bootstrap-vue.min.js",
+                "classpath*:/**/marked/**/marked.min.js",
+                "classpath*:/**/vue-multiselect/**/vue-multiselect.min.js",
+                "classpath*:/**/momentjs/**/moment.min.js",
+                "classpath*:/**/moment-duration-format/**/moment-duration-format.js"
+        )
+    }
+}
+
---
+++ src/test/java/io/codeka/gaia/dashboard/controller/UiExtensionsScannerTest.kt
@@ -0,0 +1,49 @@
+package io.codeka.gaia.dashboard.controller
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.rnorth.visibleassertions.VisibleAssertions.assertThrows
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import java.util.NoSuchElementException
+
+@SpringBootTest(classes = [UIExtensionsConfig::class])
+class UiExtensionsScannerTest(val applicationContext: ApplicationContext) {
+
+    @Test
+    fun `scan should find all exsting extensions`() {
+        val scanner = UiExtensionsScanner(applicationContext)
+
+        val locations = arrayOf(
+                // CSS
+                "classpath*:/**/bootstrap/**/bootstrap.min.css",
+                "classpath*:/**/font-awesome/**/all.css",
+                "classpath*:/**/bootstrap-vue/**/bootstrap-vue.min.css",
+                "classpath*:/**/vue-multiselect/**/vue-multiselect.min.css",
+
+                // JS
+                "classpath*:/**/jquery/**/jquery.min.js",
+                "classpath*:/**/popper.js/**/umd/popper.min.js",
+                "classpath*:/**/bootstrap/**/bootstrap.min.js",
+                "classpath*:/**/vue/**/vue.min.js",
+                "classpath*:/**/bootstrap-vue/**/bootstrap-vue.min.js",
+                "classpath*:/**/marked/**/marked.min.js",
+                "classpath*:/**/vue-multiselect/**/vue-multiselect.min.js",
+                "classpath*:/**/momentjs/**/moment.min.js",
+                "classpath*:/**/moment-duration-format/**/moment-duration-format.js"
+        )
+
+        val uiExtension = scanner.scan(*locations)
+
+        assertThat(uiExtension.size).isEqualTo(locations.size);
+    }
+
+    @Test
+    fun `scan should throw an exception when an extension cannot be found`() {
+        val scanner = UiExtensionsScanner(applicationContext)
+
+        val locations = arrayOf("classpath*:/**/bootstrap/**/bootstrap.min.BOUM")
+
+        assertThrows("Array is empty", NoSuchElementException::class.java) { scanner.scan(*locations) }
+    }
+}
