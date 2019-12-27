572ba290b1510e7ee248a0896ade86135f826dad
Julien WITTOUCK <julien.wittouck@gmail.com>
2019-12-27 18:42:38 +0100

    :recycle: : move MongoRepositories configuration to the right class

--- src/main/java/io/codeka/gaia/config/MongoConfig.java
+++ src/main/java/io/codeka/gaia/config/MongoConfig.java
@@ -3,8 +3,10 @@ package io.codeka.gaia.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "io.codeka.gaia")
 public class MongoConfig {
 
     @Autowired
