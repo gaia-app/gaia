d977e8b16518d8a3043d9390864dcd86e1c129c7
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-01-09 08:31:25 +0100

    :bug: : correct Gitlab url pattern

--- src/main/java/io/codeka/gaia/registries/gitlab/GitLabRawContent.kt
+++ src/main/java/io/codeka/gaia/registries/gitlab/GitLabRawContent.kt
@@ -2,11 +2,13 @@ package io.codeka.gaia.registries.gitlab
 
 import io.codeka.gaia.registries.RegistryType
 import io.codeka.gaia.registries.RegistryRawContent
+import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import java.util.regex.Pattern
 
+@Component
 class GitLabRawContent(restTemplate: RestTemplate) : RegistryRawContent(RegistryType.GITLAB, restTemplate) {
 
-    override val pattern: Pattern = Pattern.compile("^(http[s]?://[www.]?gitlab.*).git$")
+    override val pattern: Pattern = Pattern.compile("^https?://(w{3}\\.)?gitlab.*(.git)?$")
 
 }
