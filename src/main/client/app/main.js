17eb2a2841e9f102a10ba33bca83bf524294713b
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-02-16 14:42:23 +0100

    🔧 : configure vue-fontawesone

--- src/main/client/app/main.js
+++ src/main/client/app/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue';
+
+import fontawesomeConfig from '@/shared/config/fontawesome-config';
+
 import App from './app.vue';
 import router from './app-routes';
+
+fontawesomeConfig.init();
 
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    render: (h) => h(App),
+  router,
+  render: (h) => h(App),
 }).$mount('#app');
