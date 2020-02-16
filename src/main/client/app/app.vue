6a6b670c5b55d8e5f8262ff52e81a8c918f2771d
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-02-16 14:43:42 +0100

    🔧 : configure vue-multiselect

--- src/main/client/app/app.vue
+++ src/main/client/app/app.vue
@@ -8,4 +8,5 @@
     /* vendor css */
     @import '~bootstrap/dist/css/bootstrap.min.css';
     @import '~bootstrap-vue/dist/bootstrap-vue.min.css';
+    @import '~vue-multiselect/dist/vue-multiselect.min.css';
 </style>
