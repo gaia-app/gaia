cc29e45204f912c9a41cd5178c02236c25e7c828
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-03-07 16:27:30 +0100

    ✨ : add default layout

--- src/main/client/app/shared/components/index.js
+++ src/main/client/app/shared/components/index.js
@@ -1,2 +1,4 @@
 export { default as AppPageNotFound } from './page-not-found.vue';
 export { default as AppPageForbidden } from './page-forbidden.vue';
+export { default as AppTopBar } from './topbar/top-bar.vue';
+export { default as AppSideBar } from './sidebar/side-bar.vue';
--- src/main/client/app/shared/config/fontawesome-config.js
+++ src/main/client/app/shared/config/fontawesome-config.js
@@ -1,13 +1,23 @@
 import Vue from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
+  faAngleDoubleLeft,
+  faAngleDoubleRight,
+  faCog,
+  faLayerGroup,
   faLock,
+  faObjectGroup,
+  faSignOutAlt,
+  faTachometerAlt,
+  faTag,
   faUser,
+  faUserFriends,
 } from '@fortawesome/free-solid-svg-icons';
 import {
   faGithub,
   faGitlab,
 } from '@fortawesome/free-brands-svg-icons';
+import { faUser as farUser } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 export default {
@@ -17,6 +27,16 @@ export default {
       faLock,
       faGithub,
       faGitlab,
+      faTachometerAlt,
+      faObjectGroup,
+      faLayerGroup,
+      faCog,
+      faUserFriends,
+      faTag,
+      faSignOutAlt,
+      farUser,
+      faAngleDoubleLeft,
+      faAngleDoubleRight,
     );
     Vue.component('font-awesome-icon', FontAwesomeIcon);
   },
