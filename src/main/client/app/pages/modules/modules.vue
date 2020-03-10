c9b6f1f34b7910e3b49a66ad139c1a1ff2a31879
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-03-10 13:31:01 +0100

    :recycle: : migrate the modules page

--- src/main/client/app/pages/modules/modules.vue
+++ src/main/client/app/pages/modules/modules.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>This is not the final modules page xD</div>
+  <b-card-group columns>
+    <b-card
+      v-for="module in modules"
+      :key="module.id"
+      :title="module.name"
+    >
+      <b-card-text>
+        <app-cli-badge
+          :cli="module.terraformImage"
+          badge-style="flat-square"
+          style="margin-bottom: .75rem"
+        />
+        <p>{{ module.description }}</p>
+
+        <p v-if="module.estimatedMonthlyCost">
+          Estimated monthly cost :
+          <b-badge variant="info">
+            {{ module.estimatedMonthlyCost }} $
+          </b-badge>
+        </p>
+      </b-card-text>
+
+      <b-button
+        :to="'/modules/'+module.id"
+        title="Edit this module"
+        variant="primary"
+        class="mr-1"
+      >
+        <font-awesome-icon icon="edit" />
+      </b-button>
+
+      <b-button
+        :to="'/modules/'+module.id+'/description'"
+        title="Detail of this module"
+        variant="primary"
+        class="mr-1"
+      >
+        <font-awesome-icon icon="info" />
+      </b-button>
+
+      <b-button
+        :to="'/modules/'+module.id+'/run'"
+        title="Run this module"
+        variant="primary"
+        class="mr-1"
+      >
+        <font-awesome-icon icon="rocket" />
+      </b-button>
+    </b-card>
+  </b-card-group>
 </template>
 
 <script>
+  import axios from 'axios';
+  import { AppCliBadge } from '@/shared/components';
+
   export default {
     name: 'AppModules',
+
+    components: {
+      AppCliBadge,
+    },
+
+    data: function data() {
+      return {
+        modules: [],
+      };
+    },
+
+    async created() {
+      const response = await axios.get('/api/modules');
+      this.modules = response.data;
+    },
+
   };
 </script>
-
-<style scoped>
-
-</style>
---
+++ src/main/client/app/shared/components/cli-badge.vue
@@ -0,0 +1,28 @@
+<template>
+  <img
+    :src="'https://img.shields.io/badge/terraform-' + version + '-blue.svg?style=' + badgeStyle"
+    :alt="version"
+    :title="version"
+  >
+</template>
+
+<script>
+  export default {
+    name: 'AppCliBadge',
+    props: {
+      cli: {
+        type: Object,
+        required: true,
+      },
+      badgeStyle: {
+        type: String,
+        required: true,
+      },
+    },
+    computed: {
+      version() {
+        return this.cli.repository === 'hashicorp/terraform' ? this.cli.tag : 'custom';
+      },
+    },
+  };
+</script>
