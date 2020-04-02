7b842111b3e13e35c6078efe0fc0538d77d02bec
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-04-02 17:03:54 +0200

    ✨ : add api to get stacks

--- src/main/client/app/shared/api/stacks-api.js
+++ src/main/client/app/shared/api/stacks-api.js
@@ -5,3 +5,5 @@ export const createStack = async (stack) => axios.post('/api/stacks', stack);
 export const getStack = async (stackId) => axios.get(`/api/stacks/${stackId}`);
 
 export const saveStack = async (stack) => axios.put(`/api/stacks/${stack.id}`, stack);
+
+export const getStacks = async () => axios.get('/api/stacks');
