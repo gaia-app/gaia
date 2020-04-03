f2bc5ee85a16428be4e1af29d6bd19ac45ab9337
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-04-03 18:06:07 +0200

    ✨ : add api to get job

--- src/main/client/app/shared/api/jobs-api.js
+++ src/main/client/app/shared/api/jobs-api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const getJobs = async (stackId) => axios.get(`/api/jobs?stackId=${stackId}`);
 
+export const getJob = async (jobId) => axios.get(`/api/jobs/${jobId}`);
+
 export const retryJob = async (jobId) => axios.post(`/api/jobs/${jobId}/retry`);
 
 export const deleteJob = async (jobId) => axios.delete(`/api/jobs/${jobId}`);
