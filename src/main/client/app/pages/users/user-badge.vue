5a77b98d7c07cf811c725b31e0b606433bbf255e
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-04-03 11:32:47 +0200

    :recycle: : migrate stack edition page

---
+++ src/main/client/app/pages/users/user-badge.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="user_badge">
+    <a href="#">{{ user.username }}</a>
+    <font-awesome-icon
+      v-if="user.admin"
+      icon="user-shield"
+    />
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'AppUserBadge',
+
+    props: {
+      user: {
+        type: Object,
+        required: true,
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .user_badge a {
+    color: #3273dc !important;
+    text-decoration: none !important;
+  }
+
+  .user_badge .fa-user-shield {
+    color: #ff9900;
+  }
+</style>
--- src/main/client/app/shared/api/stacks-api.js
+++ src/main/client/app/shared/api/stacks-api.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
 
 export const createStack = async (stack) => axios.post('/api/stacks', stack);
+
+export const getStack = async (stackId) => axios.get(`/api/stacks/${stackId}`);
+
+export const saveStack = async (stack) => axios.put(`/api/stacks/${stack.id}`, stack);
