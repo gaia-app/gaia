b6f2210a6ba7604eb474811a9249919819bcffdb
Cyril Dubuisson <cyril.dubuisson@gmail.com>
2020-04-06 11:45:10 +0200

    ✨ : add api to run or destroy a stack

--- src/main/client/app/shared/api/stacks-api.js
+++ src/main/client/app/shared/api/stacks-api.js
@@ -7,3 +7,7 @@ export const getStack = async (stackId) => axios.get(`/api/stacks/${stackId}`);
 export const saveStack = async (stack) => axios.put(`/api/stacks/${stack.id}`, stack);
 
 export const getStacks = async () => axios.get('/api/stacks');
+
+export const runStack = async (stackId) => axios.post(`/api/stacks/${stackId}/RUN`);
+
+export const destroyStack = async (stackId) => axios.post(`/api/stacks/${stackId}/DESTROY`);
