a6821f401c667f9af036d08a460630a9db58fd18
Julien WITTOUCK <julien.wittouck@gmail.com>
2020-07-28 22:46:11 +0200

    :sparkles: : add NoOpEncryptionService
    This way, we can always rely on a encryption service, even if it does
    nothing at all.
    It will allow removal of null checks un CredentialsService.

--- src/main/java/io/gaia_app/encryption/EncryptionService.kt
+++ src/main/java/io/gaia_app/encryption/EncryptionService.kt
@@ -1,5 +1,9 @@
 package io.gaia_app.encryption
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
 /**
  * Service for encryption/decryption of data
  */
@@ -12,5 +16,26 @@ interface EncryptionService {
     fun decrypt(cipherText: String): String
 
     fun decryptBatch(cipherText: List<String>): List<String>
+
+}
+
+@Configuration
+class NoOpEncryptionServiceConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(EncryptionService::class)
+    fun noopEncryptionService() = NoOpEncryptionService()
+
+}
+
+class NoOpEncryptionService:EncryptionService{
+
+    override fun encrypt(plaintext: String) = plaintext
+
+    override fun encryptBatch(plaintext: List<String>) = plaintext
+
+    override fun decrypt(cipherText: String) = cipherText
+
+    override fun decryptBatch(cipherText: List<String>) = cipherText
 
 }
