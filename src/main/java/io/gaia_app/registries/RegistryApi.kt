407955e3e98090df610b409ab95e211cbb0cc0de
Julien WITTOUCK <julien.wittouck@gmail.com>
2022-03-04 17:03:55 +0100

    ✨ : compute registry details for Gitlab urls

--- src/main/java/io/gaia_app/registries/RegistryApi.kt
+++ src/main/java/io/gaia_app/registries/RegistryApi.kt
@@ -6,7 +6,15 @@ interface RegistryApi<T> {
 
     fun getRepositories(user: User) : List<T>
 
+    /**
+     * Gets a repository data for an authenticated user
+     */
     fun getRepository(user: User, projectId: String): T
+
+    /**
+     * Gets a repository data for an anonymous user
+     */
+    fun getRepository(projectId: String): T
 
     fun getFileContent(user: User, projectId: String, filename: String): String
 
--- src/main/java/io/gaia_app/registries/gitlab/GitlabRegistryApi.kt
+++ src/main/java/io/gaia_app/registries/gitlab/GitlabRegistryApi.kt
@@ -6,13 +6,20 @@ import io.gaia_app.registries.RegistryType
 import io.gaia_app.registries.SourceRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
+import java.net.URLEncoder
+import java.nio.charset.Charset
 
 @Service
 class GitlabRegistryApi(restTemplate: RestTemplate): AbstractRegistryApi<GitlabRepository>(
         restTemplate,
         RegistryType.GITLAB,
         GitlabRepository::class.java,
-        Array<GitlabRepository>::class.java)
+        Array<GitlabRepository>::class.java) {
+
+    override fun getRepository(projectId: String): GitlabRepository {
+        return super.getRepository(URLEncoder.encode(projectId, Charset.defaultCharset()))
+    }
+}
 
 /**
  * Gitlab source repository implementation
